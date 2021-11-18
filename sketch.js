19ba14e5e0a3167fac68707ac284cb0068d8f06b
eilc2008 <eilc2008567@gmail.com>
2021-11-17 19:52:36 -0600

    .

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var balloon,balloonImage1,balloonImage2;
 // crea aquí la base de datos y la variable de posición 
+var database, position;
 
 function preload(){
    bg =loadImage("cityImage.png");
@@ -18,6 +19,9 @@ function setup() {
   balloon.addAnimation("hotAirBalloon",balloonImage1);
   balloon.scale=0.5;
 
+  var balloonPosition = database.ref('balloon/height');
+  balloonPosition.on("value",readHeight, showError);
+
   textSize(20); 
 }
 
@@ -28,23 +32,49 @@ function draw() {
   if(keyDown(LEFT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //escribe el código para mover el globo aerostático en dirección hacia la izquierda
+    updateHeight(-10,0);
   }
   else if(keyDown(RIGHT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //escribe el código para mover el globo aerostático en dirección hacia la derecha
+    updateHeight(10,0);
   }
   else if(keyDown(UP_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //escribe el código para mover el globo aerostático en dirección ascendente
+    updateHeight(0,-10);
+    balloon.scale = balloon.scale +0.01;
   }
   else if(keyDown(DOWN_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
     //escribe el código para mover el globo aerostático en dirección descendente
+    updateHeight(0,10);
+    balloon.scale = balloon.scale -0.01;
   }
+
+  console.log(balloon.x);
+  console.log(balloon.y);
 
   drawSprites();
   fill(0);
   stroke("white");
   textSize(25);
   text("**¡Utiliza las teclas de flecha para mover el globo aerostático!",40,40);
+}
+
+function readHeight(data){
+  height = data.val();
+  balloon.x = height.x;
+  balloon.y = height.y;
+}
+
+function showError(){
+  console.log("Error - database");
+}
+
+function updateHeight(x,y){
+  database.ref('balloon/height').set({
+    'x': height.x + x,
+    'y': height.y + y
+  })
 }
